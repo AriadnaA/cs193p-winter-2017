b4fd44b9bd569b9142be2c83e9b6265664a0b4db
Kate Arapova <katearapova@gmail.com>
2017-05-18 15:25:08 +0300

    lecture 4 demo - FaceIt

---
+++ FaceIt/FaceIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Kateryna Arapova on 22.04.17.
+//  Copyright © 2017 cs193p. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
